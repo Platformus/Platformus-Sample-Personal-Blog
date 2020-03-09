2f9a1ec82093acda9ffcbb2056a76c0ccb29dc3b
Dmitry Sikorsky <dmitry.sikorsky@outlook.com>
2020-03-09 18:57:06 +0200

    Moved to Platformus 1.1.0

--- src/WebApplication/Startup.cs
+++ src/WebApplication/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Platformus.WebApplication.Extensions;
 
 namespace WebApplication
@@ -15,32 +16,29 @@ namespace WebApplication
     private IConfiguration configuration;
     private string extensionsPath;
 
-    public Startup(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+    public Startup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
     {
       this.configuration = configuration;
 
       if (!string.IsNullOrEmpty(this.configuration["Extensions:Path"]))
-        this.extensionsPath = hostingEnvironment.ContentRootPath + this.configuration["Extensions:Path"];
+        this.extensionsPath = webHostEnvironment.ContentRootPath + this.configuration["Extensions:Path"];
     }
 
     public void ConfigureServices(IServiceCollection services)
     {
       services.Configure<StorageContextOptions>(options =>
-      {
-        options.ConnectionString = this.configuration.GetConnectionString("Default");
-      }
+        {
+          options.ConnectionString = this.configuration.GetConnectionString("Default");
+        }
       );
 
       services.AddPlatformus(this.extensionsPath);
     }
 
-    public void Configure(IApplicationBuilder applicationBuilder, IHostingEnvironment hostingEnvironment)
+    public void Configure(IApplicationBuilder applicationBuilder, IWebHostEnvironment webHostEnvironment)
     {
-      if (hostingEnvironment.IsDevelopment())
-      {
+      if (webHostEnvironment.IsDevelopment())
         applicationBuilder.UseDeveloperExceptionPage();
-        applicationBuilder.UseDatabaseErrorPage();
-      }
 
       applicationBuilder.UsePlatformus();
     }
